4d02dc016674053dfc4b4e4cf964c0606243b713
PaulineHK <62719678+PaulineHK@users.noreply.github.com>
2020-05-10 06:22:39 +0300

    Add files via upload

---
+++ lab3/3.2/3.2(form).Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.MaxLength = 200;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(278, 124);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(296, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(278, 124);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Проверка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(406, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Задание";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(254, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Стереть всё";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(12, 171);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(562, 105);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 288);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "3.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ lab3/3.2/3.2(form).cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._2
+{
+    public partial class Form1 : Form
+    {
+        string Str;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            textBox3.Text += "Сформируйте в текстовом поле TextBox строку согласно следующим правилам:\r\n" +
+                   "– исходный текст состоит из строки, содержащей не более 200 символов;\r\n" +
+                   "– в конце каждого предложения есть точка;\r\n" +
+                   "– каждому слову, кроме первого, предшествует один пробел;\r\n" +
+                   "– внутри слов пробелов нет;\r\n" +
+                   "– знаки препинания, если они есть, пишутся сразу после слова.";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Str = textBox1.Text;
+            textBox3.Clear();
+            bool check = true;
+            if (Str == String.Empty)
+            {
+                check = false;
+                textBox3.Text += Environment.NewLine + "Строка пуста";
+            }
+            else
+            {
+             /*
+                if (Str.Length > 200)
+                {
+                    check = false;
+                    textBox3.Text += Environment.NewLine + "Сообщение содержит больше 200 символов";
+                }
+            */
+                if (Str[0] == ' ')
+                {
+                    check = false;
+                    textBox3.Text += Environment.NewLine + "Перед первым словом не должно быть пробелов.";
+                    if (!char.IsUpper(Str[1]))
+                    {
+
+                        textBox3.Text += Environment.NewLine + "Первое слово не с большой буквы";
+                    }
+
+                }
+                else if (!char.IsUpper(Str[0]))
+                {
+                    check = false;
+                    textBox3.Text += Environment.NewLine + "Первое слово не с большой буквы";
+                }
+
+                try
+                {
+                    for (int i = 0; i < Str.Length; i++)
+                    {
+                        if ((Str[i] == '.' || Str[i] == ',' || Str[i] == ':') && Str[i - 1] == ' ')
+                        {//Проверка на пробел после знака 
+                            check = false;
+                            textBox3.Text += Environment.NewLine + 
+                                "Лишний пробел между словом и знаком препинания";
+                        }
+                        if (Str[i] == '!' || Str[i] == '?' || Str[i] == ';')
+                        { //Проверка на точки
+
+                            check = false;
+                            textBox3.Text += Environment.NewLine + 
+                                "Любое предложение должно заканчиваться точкой";
+                        }
+                        if (Str[i] == '.' && i != Str.Trim().Length - 1 && Str[i + 1] != ' ')
+                        {//Проверка на пробел после точки
+                            check = false;
+                            textBox3.Text += Environment.NewLine + 
+                                "После точки не следует пробел";
+                        }
+                        else if (Str[i] == '.' && i != Str.Trim().Length - 1 && !char.IsUpper(Str[i + 2]))
+                        {//Проверка на заглавную букву 
+                            check = false;
+                            textBox3.Text += Environment.NewLine + 
+                                "Предложение не начинается с большой буквы";
+                        }
+                        if (Str[i] == ' ' && i != Str.Trim().Length - 1 && Str[i + 1] == ' ')
+                        {//проверка на пробелы между словами
+                            check = false;
+                            textBox3.Text += Environment.NewLine + "Лишний пробел между словами";
+                        }
+
+                    }
+                }
+                catch (System.IndexOutOfRangeException) { }
+            }
+            if(!check)textBox3.Text += Environment.NewLine + "Сформируйте в текстовом поле TextBox " +
+                "строку согласно следующим правилам:\r\n" +
+                  "– исходный текст состоит из строки, содержащей не более 200 символов;\r\n" +
+                  "– в конце каждого предложения есть точка;\r\n" +
+                  "– каждому слову, кроме первого, предшествует один пробел;\r\n" +
+                  "– внутри слов пробелов нет;\r\n" +
+                  "– знаки препинания, если они есть, пишутся сразу после слова.";
+
+            if (check)
+            {
+                textBox3.Text +="Вариант 2. Если длина строки L больше 15 символов, " +
+                    "то удаляется подстрока в [ ] скобках.\r\nРазделить слова запятыми.";
+            }
+            button2.Enabled = true;
+        }    
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            //textBox3.Clear();
+            textBox2.Clear();           
+            if (Str.Trim().Length > 15)
+            {
+                bool ok = false;
+                for (int i = 0; i < Str.Length; i++)
+                {
+                    if (Str[i] == '[')
+                    {
+                        ok = true;
+
+                        for (int j = i + 1; j < Str.Length; j++)
+                        {
+                            if (Str[j] == ']')
+                            {
+                                ok = false;
+                                Str = Str.Remove(i + 1, j - i - 1);
+                                break;
+                            }
+                        }
+                        if (ok)
+                        {
+                            textBox3.Text += Environment.NewLine + "Скобка ] не была найдена";
+                            break;
+                        }
+                        ok = true;
+                    }
+
+                }
+                if (!ok) textBox3.Text += Environment.NewLine + "Скобка [ не была найдена";
+            }
+            else textBox3.Text += Environment.NewLine + "Длина строки меньше 15 символов.";
+            for (int i = 0; i < Str.Trim().Length; i++)
+            {
+                if ((Str[i] == ' ') && (Str[i - 1] != '.' && Str[i - 1] != ',' && Str[i - 1] != ':'))
+                {
+                    Str = Str.Insert(i, ",");
+                }
+            }
+            textBox2.Text += Environment.NewLine + Str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
